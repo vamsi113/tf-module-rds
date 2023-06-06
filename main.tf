d48727823910eb355f679d8dcb76d29ac127f5b6
vamsi113 <krishultimate5@gmail.com>
2023-06-06 12:57:53 -0400

    change

--- main.tf
+++ main.tf
@@ -1,30 +1,29 @@
 resource "aws_rds_cluster" "default" {
   depends_on                      = [aws_rds_cluster_parameter_group.default, aws_db_subnet_group.default]
-  for_each                        = var.rds
-  cluster_identifier              = "${var.env}-${each.key}-roboshop-rds"
-  engine                          = each.value.engine
-  engine_version                  = each.value.version
+
+  cluster_identifier              = "${var.env}-${var.name}-roboshop-rds"
+  engine                          = var.engine
+  engine_version                  = var.engine_version
   database_name                   = "mydb"
   master_username                 = "foo"
   master_password                 = "bar"
-  skip_final_snapshot             = true
-  db_cluster_parameter_group_name = "${var.env}-${each.key}-roboshop-rds"
-  db_subnet_group_name            = "${var.env}-${each.key}-roboshop-rds"
+  skip_final_snapshot             = var.skip_final_snapshot
+  db_cluster_parameter_group_name = "${var.env}-${var.name}-roboshop-rds"
+  db_subnet_group_name            = "${var.env}-${var.name}-roboshop-rds"
 }
 
 resource "aws_rds_cluster_parameter_group" "default" {
-  for_each    = var.rds
-  name        = "${var.env}-${each.key}-roboshop-rds"
+
+  name        = "${var.env}-${var.name}-roboshop-rds"
   family      = "aurora5.6"
   description = "RDS default cluster parameter group"
 }
 
 resource "aws_db_subnet_group" "default" {
-  for_each   = var.rds
-  name       = "${var.env}-${each.key}-roboshop-rds"
+  name       = "${var.env}-${var.name}-roboshop-rds"
   subnet_ids = var.subnets
 
   tags = {
-    Name = "${var.env}-${each.key}-roboshop-rds"
+    Name = "${var.env}-${var.name}-roboshop-rds"
   }
 }
--- vars.tf
+++ vars.tf
@@ -1,3 +1,9 @@
-variable "rds" {}
 variable "env" {}
 variable "subnets" {}
+variable "allocated_storage" {}
+variable "engine" {}
+variable "engine_version" {}
+variable "instance_class" {}
+variable "parameter_group_name" {}
+variable "skip_final_snapshot" {}
+variable "name" {}
