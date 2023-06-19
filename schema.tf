8c4a089c0869d6974ee25118b066d31e09bafd6d
vamsi113 <krishultimate5@gmail.com>
2023-06-19 13:32:11 -0400

    change

--- schema.tf
+++ schema.tf
@@ -1,15 +1,13 @@
-#resource "null_resource" "load-schema" {
-#  depends_on = [aws_rds_cluster.default]
-#
-#  provisioner "local-exec" {
-#    command = <<EOF
-#curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-#cd /tmp
-#unzip -o mysql.zip
-#cd mysql-main
-#curl -L -O https://truststore.pki.rds.amazonaws.com/us-east-1/us-east-1-bundle.pem
-#mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile us-east-1-bundle.pem --username ${local.DOCDB_USER} --password ${local.DOCDB_PASS} < catalogue.js
-#mongo --ssl --host ${aws_docdb_cluster.docdb.endpoint}:27017 --sslCAFile us-east-1-bundle.pem --username ${local.DOCDB_USER} --password ${local.DOCDB_PASS} < users.js
-#EOF
-#  }
-#}
+resource "null_resource" "load-schema" {
+  depends_on = [aws_rds_cluster.default]
+
+  provisioner "local-exec" {
+    command = <<EOF
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
+cd /tmp
+unzip -o mysql.zip
+cd mysql-main
+mysql -h ${aws_rds_cluster.default.endpoint} -u${local.RDS_USER} -p${local.RDS_PASS} <shipping.sql
+EOF
+  }
+}
