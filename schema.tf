2a7b05e97b4c408abc593b5785a672a89f4e5b64
vamsi113 <krishultimate5@gmail.com>
2023-06-22 14:47:16 -0400

    change

--- schema.tf
+++ schema.tf
@@ -3,6 +3,7 @@ resource "null_resource" "load-schema" {
 
   provisioner "local-exec" {
     command = <<EOF
+sleep 60
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
 cd /tmp
 unzip -o mysql.zip
