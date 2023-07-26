d62a86aac4d9651004e33d61ee61e707c5b032be
vamsi113 <krishultimate5@gmail.com>
2023-07-26 14:33:59 -0400

    change1

--- schema.tf
+++ schema.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "load-schema" {
-  depends_on = [aws_rds_cluster.default]
+  depends_on = [aws_rds_cluster.default, aws_rds_cluster_instance.cluster_instances, aws_rds_cluster_parameter_group.default,aws_db_subnet_group.default]
 
   provisioner "local-exec" {
     command = <<EOF
